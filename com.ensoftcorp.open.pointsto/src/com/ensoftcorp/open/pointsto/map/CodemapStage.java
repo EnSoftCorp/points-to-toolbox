03e514b1d8ce1136d526cb571bf6c56ae7ab6496
Ben Holland <benjholla@gmail.com>
2016-02-07 12:00:41 -0600

    added preferences page to enable/disable jimple indexing

--- com.ensoftcorp.open.pointsto/src/com/ensoftcorp/open/pointsto/map/CodemapStage.java
+++ com.ensoftcorp.open.pointsto/src/com/ensoftcorp/open/pointsto/map/CodemapStage.java
@@ -11,6 +11,7 @@ import com.ensoftcorp.atlas.core.query.Q;
 import com.ensoftcorp.atlas.core.script.Common;
 import com.ensoftcorp.open.pointsto.analysis.JimplePointsTo;
 import com.ensoftcorp.open.pointsto.common.Constants;
+import com.ensoftcorp.open.pointsto.ui.PointsToPreferences;
 import com.ensoftcorp.open.pointsto.utilities.GraphEnhancements;
 
 /**
@@ -29,31 +30,33 @@ public class CodemapStage implements ToolboxIndexingStage {
 	@Override
 	public void performIndexing(IProgressMonitor monitor) {
 		try {
-			JimplePointsTo jimplePointsTo = new JimplePointsTo();
-			jimplePointsTo.run();
-			
-			// make some graph enhancements
-			GraphEnhancements.rewriteArrayComponents();
-			GraphEnhancements.tagInferredEdges();
-			
-			// TODO: Pester Ensoft for a better workaround
-			// SUPER NASTY HACK!!! Storing results in an isolated graph node
-			// because class loaders are a pain in the butt...
-//			PointsToResults pointsToResults = PointsToResults.getInstance();
-//			pointsToResults.addressToInstantiation = jimplePointsTo.getAddressToInstantiation();
-//			pointsToResults.addressToType = jimplePointsTo.getAddressToType();
-//			pointsToResults.arrayMemoryModel = jimplePointsTo.getArrayMemoryModel();
-//			pointsToResults.inferredDataFlowGraph = jimplePointsTo.getInferredDataFlowGraph();
-			GraphElement results = Graph.U.createNode();
-			results.tags().add("points-to-results");
-			results.putAttr("addressToInstantiation", jimplePointsTo.getAddressToInstantiation());
-			results.putAttr("addressToType", jimplePointsTo.getAddressToType());
-			results.putAttr("arrayMemoryModel", jimplePointsTo.getArrayMemoryModel());
-			// really this should just be inferred edges, but for now just
-			// combining points-to set and connected array components graph
-			// enhancement sets adding inferred edges since some may
-			Q inferredDataFlowGraph = jimplePointsTo.getInferredDataFlowGraph().union(Common.universe().edgesTaggedWithAny(Constants.INFERRED));
-			results.putAttr("inferredDataFlowGraph", Common.resolve(new NullProgressMonitor(), inferredDataFlowGraph));
+			if(PointsToPreferences.isJimplePointsToAnalysisEnabled()){
+				JimplePointsTo jimplePointsTo = new JimplePointsTo();
+				jimplePointsTo.run();
+				
+				// make some graph enhancements
+				GraphEnhancements.rewriteArrayComponents();
+				GraphEnhancements.tagInferredEdges();
+				
+				// TODO: Pester Ensoft for a better workaround
+				// SUPER NASTY HACK!!! Storing results in an isolated graph node
+				// because class loaders are a pain in the butt...
+//				PointsToResults pointsToResults = PointsToResults.getInstance();
+//				pointsToResults.addressToInstantiation = jimplePointsTo.getAddressToInstantiation();
+//				pointsToResults.addressToType = jimplePointsTo.getAddressToType();
+//				pointsToResults.arrayMemoryModel = jimplePointsTo.getArrayMemoryModel();
+//				pointsToResults.inferredDataFlowGraph = jimplePointsTo.getInferredDataFlowGraph();
+				GraphElement results = Graph.U.createNode();
+				results.tags().add("points-to-results");
+				results.putAttr("addressToInstantiation", jimplePointsTo.getAddressToInstantiation());
+				results.putAttr("addressToType", jimplePointsTo.getAddressToType());
+				results.putAttr("arrayMemoryModel", jimplePointsTo.getArrayMemoryModel());
+				// really this should just be inferred edges, but for now just
+				// combining points-to set and connected array components graph
+				// enhancement sets adding inferred edges since some may
+				Q inferredDataFlowGraph = jimplePointsTo.getInferredDataFlowGraph().union(Common.universe().edgesTaggedWithAny(Constants.INFERRED));
+				results.putAttr("inferredDataFlowGraph", Common.resolve(new NullProgressMonitor(), inferredDataFlowGraph));
+			}
 		} catch (Exception e) {
 			Log.error("Error performing points-to analysis", e);
 		}
--- com.ensoftcorp.open.pointsto/src/com/ensoftcorp/open/pointsto/ui/PointsToPreferences.java
+++ com.ensoftcorp.open.pointsto/src/com/ensoftcorp/open/pointsto/ui/PointsToPreferences.java
@@ -1,12 +1,40 @@
 package com.ensoftcorp.open.pointsto.ui;
 
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import com.ensoftcorp.open.pointsto.Activator;
+
 /**
  * UI for setting points-to analysis preferences
  * 
  * @author Ben Holland
  */
-public class PointsToPreferences {
-
-	// TODO: implement
+public class PointsToPreferences extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 	
+	// enable/disable jimple points-to analysis
+	public static final String ENABLE_JIMPLE_POINTS_TO_ANALYSIS_BOOLEAN = "ENABLE_JIMPLE_POINTS_TO_ANALYSIS_BOOLEAN";
+	public static final String ENABLE_JIMPLE_POINTS_TO_ANALYSIS_DESCRIPTION = "Enable Jimple Points-to Analysis";
+	
+	public static boolean isJimplePointsToAnalysisEnabled(){
+		return Activator.getDefault().getPreferenceStore().getBoolean(ENABLE_JIMPLE_POINTS_TO_ANALYSIS_BOOLEAN);
+	}
+	
+	public PointsToPreferences() {
+		super(GRID);
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("Configure preferences for the Points-to Analysis Toolbox plugin.");
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		addField(new BooleanFieldEditor(ENABLE_JIMPLE_POINTS_TO_ANALYSIS_BOOLEAN, "&" + ENABLE_JIMPLE_POINTS_TO_ANALYSIS_DESCRIPTION, getFieldEditorParent()));
+	}
+
 }
