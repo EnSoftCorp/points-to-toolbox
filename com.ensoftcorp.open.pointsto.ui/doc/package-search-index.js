2a86d67795bbe0915e022098118d01279a62dedb
Benjamin Holland <benjholla@gmail.com>
2020-05-24 23:57:42 -0400

    update javadoc, copyright, version

---
+++ com.ensoftcorp.open.pointsto.ui/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.ensoftcorp.open.pointsto.ui"},{"l":"com.ensoftcorp.open.pointsto.ui.log"},{"l":"com.ensoftcorp.open.pointsto.ui.preferences"},{"l":"com.ensoftcorp.open.pointsto.ui.smart"}]
